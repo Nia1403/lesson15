3aca530ffc33068a84f3faad012d21d6e683fa59
Nia1403 <nia.nia@hotmail.de>
2024-11-15 14:35:48 +0100

    geändert

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ let weekDay = 'friday';
 let activity = 'hike';
 
 const text = 'Hi, my name is ' + saxeli + ' today is ' + weekDay + ', aperfect day for a ' + activity + '.';
+typeof saxeli;
 
-console.log(text)
+console.log(text,  'fewfwef')
 
-// console.log -> print out
+// console.log -> print out, unterschied, ein Satz kann man NICHT direcht in console log mit + und so bauen
 // var let -> gleich, var ist nur alt. variablen die geändert werden können
 // const -> variable die nicht geändert werden können, ähnlich wie final
 
