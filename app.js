f79c75fe5e0a7c89e72d86d5fc6113bcc49d32ee
Nia1403 <nia.nia@hotmail.de>
2024-11-15 15:20:23 +0100

    done

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+
+let int = undefined;
+console.log('hier undefined int type is: ', typeof int)
+
 let primitiveTypes = `
 hier are primitive tytes:
-int: ${15  + ' -> ' + typeof 15} 
+
+int: ${15  + ' -> ' + typeof 15}
 float: ${1.5  + ' -> ' + typeof 1.5}
 double: ${1.23421  + ' -> ' + typeof 1.23421}
 string: ${'Text'  + ' -> ' + typeof 'hier is a text'}
@@ -9,10 +14,41 @@ boolean: ${true  + ' -> ' + typeof true}
 `;
 console.log(primitiveTypes);
 
+// int variable changed
+// let int = 17;
+// console.log(int);
+
 
 const saxeli = 'Nia';
-let weekDay = 'friday';
 let activity = 'hike';
+let weekDay = null;
+
+let datum = new Date();
+let dayToday= datum.getDay();
+
+switch(dayToday){
+case 0:
+    weekDay = "Sunday";
+    break;
+case 1:
+    weekDay = "Monday";
+    break;
+case 2:
+    weekDay = "Thuesday";
+    break;
+case 3:
+    weekDay = "Wednesday";
+    break;
+case 4:
+    weekDay = "Thursday";
+    break;
+case 5:
+    weekDay = "Friday"
+    break;
+case 6:
+    weekDay = "Saturday"
+    break;
+}
 
 const text = 'Hi, my name is ' + saxeli + ' today is ' + weekDay + ', and a perfect day for a ' + activity + '.';
 console.log(text)
