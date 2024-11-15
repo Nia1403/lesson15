48b29a127c96cc531ab5471b3b6d09812da11eed
Nia1403 <nia.nia@hotmail.de>
2024-11-15 14:25:33 +0100

    varablen  added, and also text

---
+++ app.js
@@ -0,0 +1,24 @@
+let primitiveTypes = `
+hier are primitive tytes:
+int: ${15}
+string: ${'hier is a text'}
+float: ${1.5}
+double: ${1.23421}
+char: ${'a'}
+boolean: ${true}
+`;
+console.log(primitiveTypes);
+
+
+const saxeli = 'Nia';
+let weekDay = 'friday';
+let activity = 'hike';
+
+const text = 'Hi, my name is ' + saxeli + ' today is ' + weekDay + ', aperfect day for a ' + activity + '.';
+
+console.log(text)
+
+// console.log -> print out
+// var let -> gleich, var ist nur alt. variablen die geändert werden können
+// const -> variable die nicht geändert werden können, ähnlich wie final
+
